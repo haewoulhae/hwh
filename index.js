8c1bea98cd54173cbe95566af97f999f08f5e0ac
hongsh26 <swatch6264@gmail.com>
2023-12-19 16:25:15 +0900

    start createAccount

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function test() {
   console.log(data.value);
   data.value=""
 }
+function ad(){
+  alert("손 잘 씻기!!")
+}
+function create(){
+  const name = document.getElementsByName("name")
+  const id = document.getElementsByName("id").value
+  const pw = document.getElementsByName("password").value
+  const nickname = document.getElementsByName("nickname").value
+  const email = document.getElementsByName("mail").value
+  const answer = document.getElementsByName("question").value
+  location.href("index.html")
+}
