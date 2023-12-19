d09562b2c3edeeeaed41adb12a207859f96c0a92
hongsh26 <swatch6264@gmail.com>
2023-12-19 12:05:31 +0900

    merge ready

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 window.onload = function () {
-  alert("on!");
+
 };
 function test() {
   alert("입력 감지");
-  const data = document.getElementById("search").value;
-  console.log(data);
+  const data = document.getElementById("search");
+  console.log(data.value);
+  data.value=""
 }
