7e30b889aeabea0d2ed910eff377f6908d3bf482
hongsh26 <swatch6264@gmail.com>
2023-12-20 13:17:52 +0900

    second merge

---
+++ public/index.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  const name = document.getElementsByName("name")
+  const id = document.getElementsByName("id")
+  const pw = document.getElementsByName("password")
+  const nickname = document.getElementsByName("nickname")
+  const email = document.getElementsByName("mail")
+  const answer = document.getElementsByName("question")
+};
+function test() {
+  alert("입력 감지");
+  const data = document.getElementById("search");
+  console.log(data.value);
+  data.value=""
+}
+function ad(){
+  alert("매너 합시다 ^ㅡ^")
+}
+function create(){
+  alert("hi")
+  const data = document.getElementsByName("name")
+  console.log(data.value)
+}
